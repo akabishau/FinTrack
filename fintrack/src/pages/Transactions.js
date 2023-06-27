dadcb5ad1c717893ba279ee4c45084077b92f4f1
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-06-27 11:38:40 -0400

    display transactions imported from the data sample

--- fintrack/src/pages/Transactions.js
+++ fintrack/src/pages/Transactions.js
@@ -1,6 +1,52 @@
+import transactionData from '../data/transactions.json'
+
+const TransactionListHeader = () => {
+    return (
+        <header>
+            <div>Type</div>
+            <div> Category</div>
+            <div> Amount</div>
+        </header>
+    )
+}
+
+// { } is used to insert JS expression into JSX, should return a value
+const Transaction = (props) => {
+    return (
+        <div>
+            <div>{props.type}</div>
+            <div>{props.category}</div>
+            <div>{'$' + props.amount}</div>
+        </div>
+    )
+}
+
+
+const TransactionList = (props) => {
+    return (
+        <div>
+            <TransactionListHeader />
+            {/* transactions list */}
+            {props.transactions.map(transaction =>
+                <Transaction
+                    key={transaction._id}
+                    type={transaction.transType}
+                    category={transaction.category}
+                    amount={transaction.amount} />
+            )}
+        </div>
+    )
+}
+
+
 const TransactionsPage = () => {
     return (
-        <h1>This is the all transactions page!</h1>
+        <>
+            <h1>Recent Transactions</h1>
+            <TransactionListHeader />
+            <TransactionList transactions={transactionData.transactions} />
+        </>
+        
     )
 }
 
