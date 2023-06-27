aaaebb6e3b0e51afad7e65ae2adc64f74137b1e0
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-06-27 12:07:00 -0400

    extract transaction list component and add navigation to selected transaction

--- fintrack/src/pages/Transactions.js
+++ fintrack/src/pages/Transactions.js
@@ -1,49 +1,10 @@
 import transactionData from '../data/transactions.json'
-
-const TransactionListHeader = () => {
-    return (
-        <header>
-            <div>Type</div>
-            <div> Category</div>
-            <div> Amount</div>
-        </header>
-    )
-}
-
-// { } is used to insert JS expression into JSX, should return a value
-const Transaction = (props) => {
-    return (
-        <div>
-            <div>{props.type}</div>
-            <div>{props.category}</div>
-            <div>{'$' + props.amount}</div>
-        </div>
-    )
-}
-
-
-const TransactionList = (props) => {
-    return (
-        <div>
-            <TransactionListHeader />
-            {/* transactions list */}
-            {props.transactions.map(transaction =>
-                <Transaction
-                    key={transaction._id}
-                    type={transaction.transType}
-                    category={transaction.category}
-                    amount={transaction.amount} />
-            )}
-        </div>
-    )
-}
-
+import TransactionList from '../components/TransactionList'
 
 const TransactionsPage = () => {
     return (
         <>
             <h1>Recent Transactions</h1>
-            <TransactionListHeader />
             <TransactionList transactions={transactionData.transactions} />
         </>
         
