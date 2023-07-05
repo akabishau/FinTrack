d9f601d531ebbfa94ee14c1f306037debfcfcca9
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-07-05 10:31:41 -0400

    clean up old code

--- fintrack/src/App.js
+++ fintrack/src/App.js
@@ -1,26 +1,8 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/Home'
-import TransactionsPage from './pages/Transactions'
-import TransactionPage from './pages/Transaction'
-import AboutPage from './pages/About'
-import NotFound from './pages/NotFound'
-import Layout from './components/Layout'
-
-
 const App = () => {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route exact path='/' element={<HomePage />} />
-          <Route path='/transactions' element={<TransactionsPage />} />
-          <Route path='/transactions/:transactionId' element={<TransactionPage />} />
-          <Route path='/about' element={<AboutPage />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <div>
+      <h1>FinTrack</h1>
+    </div>
   )
 }
 
